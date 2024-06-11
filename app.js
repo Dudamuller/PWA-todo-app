415c0558e96f742a5d74a74a178ce31a6172435c
Maria Eduarda Muller <162138334+Dudamuller@users.noreply.github.com>
2024-06-11 11:10:58 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById('todo-form')
+addEventListener("submit",function(e) {
+e.preveventDefault();
+const input = this.documentElementById
+("todo input");
+AudiTodo(input.value);
+imput.value = "";
+});
+function addTodo (task){
+    const list = document.getElementById("todo-list");
+    const listItem = task;
+    list appendChild(listItem);
+}
